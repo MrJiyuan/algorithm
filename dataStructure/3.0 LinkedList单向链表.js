bf89868b04e27cae0773e142ae9dde95ab2aeb19
Chr1s <370493992@qq.com>
2022-11-02 03:55:19 +0800

    linklist finfish

--- dataStructure/3.0 LinkedList单向链表.js
+++ dataStructure/3.0 LinkedList单向链表.js
@@ -1,3 +1,10 @@
+/**
+ * @ Author: Chr1s
+ * @ Create Time: 2022-10-31 03:19:19
+ * @ Modified time: 2022-11-02 03:54:52
+ * @ Description:
+ */
+
 function LinkedList() {
     // 内部类：节点类
     function Node(data, next) {
@@ -38,10 +45,10 @@ function LinkedList() {
         return ''
     }
 
-    // method:insert
+    // method:insert 在指定位置插入新节点
     LinkedList.prototype.insert = function (position, data) {
         // 对position进行越界判断
-        if (position < 0 && position > this.length) return false
+        if (position < 0 || position > this.length) return false
         var newNode = new Node(data)
         if (position == 0) { // 插入position=0的位置
             newNode.next = this.head
@@ -61,13 +68,78 @@ function LinkedList() {
         this.length += 1
         return true
     }
+
+    // method:update 修改指定位置的元素
+    LinkedList.prototype.update = function (position, newData) {
+        // 对position进行越界判断
+        if (position < 0 || position >= this.length) return false
+        var current = this.head
+        var i = 0
+        while (i++ < position) {
+            current = current.next
+        }
+        current.data = newData
+        return true
+    }
+
+    // method:get 获取指定索引的元素
+    LinkedList.prototype.get = function (position) {
+        if (position < 0 || position >= this.length) return null
+        var current = this.head
+        var i = 0
+        while (i++ < position) {
+            current = current.next
+        }
+        return current.data
+    }
+
+    // method:indexOf 返回元素在列表中的索引，若没有该元素则返回-1
+    LinkedList.prototype.indexOf = function (element) {
+        var current = this.head
+        var i = 0
+        while (i++ < this.length) {
+            if (current.data === element) {
+                return i
+            }
+            current = current.next
+        }
+        return -1
+    }
+
+    // method:removeAt 从列表的特定位置移除一项
+    LinkedList.prototype.removeAt = function (position) {
+        if (position < 0 || position >= this.length) return false
+        var current = this.head
+        var previous = null // 指定位置的前一个元素
+        // 1 若删除的是第一个元素
+        if (position === 0) {
+            this.head = this.head.next // 让head直接指向后一个节点
+        } else {// 2 若删除的是中间的元素
+            var i = 0
+            while (i++ < position) {
+                previous = current
+                current = current.next
+            }
+            previous.next = current.next
+        }
+        // 长度减一
+        this.length -= 1
+        return current.data
+    }
+
+    // method:remove   从列表中移除一项
+    LinkedList.prototype.remove = function (element) {
+        return this.removeAt(
+            this.indexOf(element)
+        )
+    }
+
 }
 // method:testToString
 var list = new LinkedList()
+// 1.测试append
 list.append('abc')
 list.append('def')
-list.insert(1, 'ghi')
-console.log(list);
 // LinkedList {
 //     head: Node {
 //         data: 'abc',
@@ -75,3 +147,19 @@ console.log(list);
 //     },
 //     length: 2
 // }
+
+// 2.测试insert
+// list.insert(0, 'aaa')
+// list.insert(3, 'bbb')
+// list.insert(5, 'ccc')
+
+// 3.测试get
+// console.log(list.get(5));
+
+// 4.测试indexOf
+// console.log(list.indexOf('ccc'));
+
+
+console.log(list.removeAt(1));
+console.log(list);
+

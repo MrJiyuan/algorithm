c8526c68fbfcb3b9f9fce083be0ebd40cfffdf33
Chr1s <370493992@qq.com>
2023-02-27 23:18:24 +0800

    21.0

--- dataStructure/leetcode/21.0 合并两个有序链表Merge Two Sorted Lists.ts
+++ dataStructure/leetcode/21.0 合并两个有序链表Merge Two Sorted Lists.ts
@@ -20,19 +20,27 @@
 // -100 <= Node.val <= 100
 // l1 和 l2 均按 非递减顺序 排列
 
-
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+// 终止条件：当两个链表都为空时，表示我们对链表已合并完成。
+// 如何递归：判断 l1 和 l2 头结点哪个更小，然后较小结点的 next 指针指向其余结点的合并结果。（调用递归）
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+    if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
 };
+// 执行用时：72 ms, 在所有 TypeScript 提交中击败了68.26%的用户
+// 内存消耗：43.9 MB, 在所有 TypeScript 提交中击败了48.61%的用户
